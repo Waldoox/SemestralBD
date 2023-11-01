c23a9d50a98b462768f4addff4e8b1109f39be4a
Waldoox <kevin.barria1@utp.ac.pa>
2023-11-01 10:10:49 -0500

    Agregue el trigger para asignar el codigo de proyecto
    Corregí algunos errores y añadí el trigger para asignar cod_proyecto

--- trabajo_graduacion.sql
+++ trabajo_graduacion.sql
@@ -1,13 +1,23 @@
 create database trabajos_graduacion
 
-use trabajos_graduacion
 
 create table sede
 (
-	cod_sede int
-		constraint SEDE_COD_SEDE_PK primary key (cod_sede),
-	nombre_sede varchar(25),
-	ubicacion varchar(50)
+cod_sede int
+constraint SEDE_COD_SEDE_PK primary key (cod_sede)
+constraint sede_cod_sede_ck check 
+(cod_sede like '[0][1-8]'),
+
+nombre_sede varchar(50)
+constraint sede_nombre_sede_ck check
+(nombre_sede in ('Campus Victor Levi Sasso','Centro Regional de Colón','Centro Regional de Panamá Oeste',
+'Centro Regional de Coclé', 'Centro Regional de Azuero','Centro Regional de Veraguas','Centro Regional de Chiriquí',
+'Centro Regional de Bocas del Toro')),
+
+ubicacion varchar(25) 
+constraint sede_ubicacion_ck check
+(ubicacion in ('Panamá','colón','Panamá Oeste', 'Coclé','Veraguas','Chiriquí','Bocas del Toro'))
+
 )
 
 create table empresa
@@ -52,63 +62,80 @@ create table profesor
 
 create table departamento
 (
-	cod_depto int 
+	cod_depto int identity
 		constraint DEPARTAMENTO_COD_DEPTO_PK primary key(cod_depto),
-	nombre_depto varchar(25),
-	cod_profesor_jefe int
-		constraint DEPARTAMENTO_COD_PROFESOR_JEFE_FK
-			foreign key(cod_profesor_jefe)
-				references profesor(cod_profesor)
-		constraint DEPARTAMENTO_COD_PROFESOR_JEFE_CK
-			check(cod_profesor_jefe = (select cod_profesor from profesor where tipo_prof = 'Jefe'))
+
+	nombre_depto varchar(40),
+
+	cod_profesor_jefe int unique 
+	
+	constraint DEPARTAMENTO_COD_PROFESOR_JEFE_FK foreign key
+	(cod_profesor_jefe) references profesor(cod_profesor)
+
+	constraint DEPARTAMENTO_COD_PROFESOR_JEFE_CK check
+	(cod_profesor_jefe = (select cod_profesor from profesor where tipo_prof = 'Jefe'))
+
 )
+
 
 alter table profesor
 	add constraint PROFESOR_COD_DEPTO_FK 
 		foreign key (cod_depto)
 			references departamento(cod_depto)
 
+
+
 create table proyecto
 (
-	cod_proyecto char(4) identity 
-		constraint PROYECTO_COD_PROYECTO_PK primary key(cod_proyecto) ,
-	titulo_proyecto varchar(50),
-	cod_profesor int
+	cod_proyecto varchar(15) 
+		constraint PROYECTO_COD_PROYECTO_PK primary key(cod_proyecto),
+	tipo_proyecto char(2),
+	titulo_proyecto varchar(50) unique,
+	fecha_entrega date 
+		constraint proyecto_fecha_entrega_ck 
+			check (fecha_entrega <= getdate()),
+	fecha_verificacion date 
+		constraint proyecto_fecha_verificacion_ck 
+			check (fecha_verificacion <= getdate() and fecha_verificacion <= fecha_entrega ) ,
+	fecha_evaluacion date 
+		constraint proyecto_fecha_evaluacion_ck 
+			check (fecha_evaluacion <= getdate() and fecha_evaluacion <= fecha_verificacion ),
+	fecha_aprobacion date 
+	constraint proyecto_fecha_aprobacion_ck 
+		check (fecha_aprobacion <= getdate() and fecha_aprobacion <= fecha_evaluacion ),
+
+	fecha_sustentacion date 
+		constraint proyecto_fecha_sustentacion_ck 
+			check (fecha_sustentacion <= getdate() and fecha_sustentacion <= fecha_aprobacion ),
+	cod_profesor int 
 		constraint PROYECTO_COD_PROFESOR_FK 
-			foreign key (cod_profesor)
+			foreign key (cod_profesor) 
 				references profesor(cod_profesor),
-	fecha_entrega date,
-	fecha_verificacion date,
-	fecha_evaluacion date,
-	fecha_aprobacion date,
-	fecha_sustentacion date
+
 )
 
-create table proyecto_profesor_sustenta
-(
-	cod_profesor
-	cod_proyecto
-	ced_estudiante
-)
+
 
 create table Evaluacion (
-    cod_proyecto char(12)
-		constraint Evaluacion_cod_proyecto_fk foreign key
-			references Proyecto(cod_proyecto),
-    fecha_evaluacion date 
-		constraint Evaluacion_fecha_evaluacion_fk foreign key 
-			references Proyecto (fecha_evaluacion),
+    cod_proyecto varchar(15)
+		constraint Evaluacion_cod_proyecto_fk 
+			foreign key (cod_proyecto)
+				references Proyecto(cod_proyecto),
+    fecha_evaluacion date,
     Evaluacion varchar,
-	Primary key(cod_proyecto, Fecha_evaluacion)
+
+		Constraint EVALUACION_CODPROYECTO_FECHAEV_PK
+			Primary key(cod_proyecto, Fecha_evaluacion)
 )
 
 create table Carrera (
-    cod_carrera int ,
+    cod_carrera int identity
+	constraint carrera_cod_carrera_pk primary key ,
     nombre_carrera varchar,
     Cod_depto int 
-		constraint Carrera_cod_depto_FK foreign key 
-			references Departamento(cod_depto)
-    primary key (cod_carrera, cod_depto)
+		constraint Carrera_cod_depto_FK 
+			foreign key(cod_depto)
+				references Departamento(cod_depto)
 )
 
 create table Estudiante(
@@ -117,18 +144,20 @@ create table Estudiante(
 		 constraint Estudiante_ced_estudiante_ck check
 		 (ced_estudiante like '[0][1-9][-][0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9][0-9]'
 		or 
-			ced_estudiante like '[1][0-3][-][0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9][0-9]'),
-    pri_nom varchar,
-    seg_nom varchar,
-    pri_apellido varchar,
-    seg_apellido varchar,
-    año_cursa int,
+			ced_estudiante like '[1][0-3][-][0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9][0-9]')
+			/* falta lo de la cedula de los extranjeros */,
+    pri_nom varchar(25),
+    seg_nom varchar(25),
+    pri_apellido varchar(25),
+    seg_apellido varchar(25),
+    año_cursa DATE,
     semestre int,
     indice float,
 
-    cod_proyecto char(12)
-		constraint Estudiante_cod_proyecto_fk foreign key 
-			references Proyecto (cod_proyecto),
+    cod_proyecto varchar(15)
+		constraint Estudiante_cod_proyecto_fk 
+			foreign key (cod_proyecto)
+				references Proyecto (cod_proyecto),
     cod_carrera int
 		constraint Estudiante_cod_carrera_fk foreign key 
 			references Carrera (cod_carrera),
@@ -137,3 +166,55 @@ create table Estudiante(
 		 references Sede(cod_sede)
 )
 
+create table proyecto_profesor_sustenta
+(
+	cod_profesor int
+	constraint proyecto_profesor_sustenta_cod_profesor_FK foreign key
+	(cod_profesor) references profesor(cod_profesor),
+
+	cod_proyecto int 
+	constraint proyecto_profesor_sustenta_cod_proyecto_FK foreign key
+	(cod_profesor) references profesor (cod_profesor),
+
+	ced_estudiante int 
+		constraint proyecto_profesor_sustenta_ced_estudiante_FK 
+		foreign key(ced_estudiante) 
+			references estudiante(ced_estudiante)
+)
+
+create sequence Numero_Proyecto
+	start with 1000
+	Increment BY 1
+
+create TRIGGER Inserta_Proyecto
+ON proyecto
+INSTEAD OF INSERT
+AS
+
+declare @tipo_proy char(2),
+		@titulo_proy varchar(50),
+		@cod_profesor int,
+		@fecha_entrega date,
+		@fecha_verificacion date,
+		@fecha_evaluacion date,
+		@fecha_aprobacion date,
+		@fecha_sustentacion date,
+		@cod_proyecto varchar(15),
+		@annio int = year(getdate()),
+		@secuencia int
+
+	select @tipo_proy = inserted.tipo_proyecto,
+		   @titulo_proy = inserted.titulo_proyecto,
+		   @fecha_entrega = inserted.fecha_entrega,
+		   @fecha_verificacion = inserted.fecha_verificacion,
+		   @fecha_evaluacion = inserted.fecha_evaluacion,
+		   @fecha_aprobacion = inserted.fecha_aprobacion,
+		   @fecha_sustentacion = inserted.fecha_sustentacion,
+		   @cod_profesor = inserted.cod_profesor
+		from inserted
+
+	set @secuencia = NEXT VALUE FOR Numero_Proyecto
+	set @cod_proyecto = CONCAT(@tipo_proy,'-',@annio,'-',@secuencia)
+
+	insert into proyecto (cod_proyecto,tipo_proyecto,titulo_proyecto,fecha_entrega,fecha_verificacion,fecha_evaluacion,fecha_aprobacion,fecha_sustentacion,cod_profesor)
+		values (@cod_proyecto, @tipo_proy,@titulo_proy,@fecha_entrega,@fecha_verificacion,@fecha_evaluacion,@fecha_aprobacion,@fecha_sustentacion,@cod_profesor)         
